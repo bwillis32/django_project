f80b52df115d913b872d8747a904c49770aa38e6
bwillis32 <bwillis@bentoncorp.com>
2018-09-17 23:17:26 -0400

    completed video 5

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,9 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': "CoreyMS",
-        "title": 'Blog Post 1',
-        'content':'First post content',
-        'date_posted': 'august 27, 2018'
-    },
-
-    {
-        'author': "Jane Doe",
-        "title": 'Blog Post 2',
-        'content':'Second post content',
-        'date_posted': 'august 28, 2018'
-    },
-]
-
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
